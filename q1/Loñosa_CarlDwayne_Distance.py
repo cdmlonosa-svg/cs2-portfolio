54211cad15945c475e3d8f344c093664dba1ca7c
cdmlonosa-svg <cdmlonosa@brc.pshs.edu.ph>
2026-08-17 21:27:31 +0800

    Update Loñosa_CarlDwayne_Distance.py

--- q1/Loñosa_CarlDwayne_Distance.py
+++ q1/Loñosa_CarlDwayne_Distance.py
@@ -10,5 +10,5 @@ print(f"The distance between the two points is: {distance}")
 #The math library simplified my program by providing me with built-in mathematical functions, which made my code simpler and easier to understand.
 #pow() helped me because it raised the difference between the x and y coordinates
 #sqrt() helped me because it computed the square root of the sum of the squared differences.
-#If pow() wasn' t available, I would've used ** 2 (example: (x2 - x1) ** 2).
-#If sqrt() wasn' t available, I would've used ** 0.5 (example: (d) ** 0.5). 22
+#If pow() wasn't available, I would've used ** 2 (example: (x2 - x1) ** 2).
+#If sqrt() wasn't available, I would've used ** 0.5 (example: (d) ** 0.5). 22
