fe6d5bfcde011ac4225e05d4600ae4f053641f78
cdmlonosa-svg <cdmlonosa@brc.pshs.edu.ph>
2026-09-09 20:36:42 +0800

    Update workshop_validator.py

--- q1/workshop_validator.py
+++ q1/workshop_validator.py
@@ -3,20 +3,20 @@ if not name:
     print("REGISTRATION NOT ACCEPTED")
     print("Student name is required.")
 else:
-    age_input = input("Enter age: ").strip()
+    age = input("Enter age: ").strip()
     try:
-        age = int(age_input)
-        if age < 11 or age > 18:
+        age_int = int(age)
+        if age_int < 11 or age_int > 18:
             print("REGISTRATION NOT ACCEPTED")
             print("Age must be from 11 to 18.")
         else:
-            grade_input = input("Enter grade level (7-12): ").strip()
+            grades = input("Enter grade level (7-12): ").strip()
             valid_grades = ["7", "8", "9", "10", "11", "12"]
-            if grade_input not in valid_grades:
+            if grades not in valid_grades:
                 print("REGISTRATION NOT ACCEPTED")
                 print("Invalid grade level.")
             else:
-                grade_level = int(grade_input)
+                grade_int = int(grades)
                 email = input("Enter email: ").strip()
                 if "@" not in email or "." not in email:
                     print("REGISTRATION NOT ACCEPTED")
@@ -27,17 +27,18 @@ else:
                         print("REGISTRATION NOT ACCEPTED")
                         print("The registration code must contain exactly 6 characters.")
                     else:
-                        print("-------------------------------")
+                        print("------------------------------")
                         print("REGISTRATION ACCEPTED")
-                        print("-------------------------------")
+                        print("------------------------------")
                         print(f"Student: {name}")
                         print(f"Age: {age}")
-                        print(f"Grade Level: {grade_level}")
+                        print(f"Grade Level: {grades}")
                         print(f"Email: {email}")
                         print(f"Registration Code: {reg_code}")
     except ValueError:
         print("REGISTRATION NOT ACCEPTED")
         print("Age must be a number.")
-      
-#LEARNING RESOURCE:
+
+#ADDITIONAL LEARNING RESOURCE
 #Code, B. [Bro Code]. (2024, June 29). Learn Python EXCEPTION HANDLING in 5 minutes! 🚦 [Video]. YouTube. https://www.youtube.com/watch?v=V_NXT2-QIlE
+
