96f3a62beaac47e1348da188b0a5bff5b6322d3e
cdmlonosa-svg <cdmlonosa@brc.pshs.edu.ph>
2026-09-23 20:54:13 +0800

    Update secure_registration.py

--- q1/secure_registration.py
+++ q1/secure_registration.py
@@ -1,1 +1,6 @@
-
+name = input("Enter student name: ").strip()
+if not name:
+    print("REGISTRATION NOT ACCEPTED")
+    print("Student name is required.")
+else:
+    age = input("Enter age: ").strip()
