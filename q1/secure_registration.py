187b91e2d392706a3b1deb6f8dbc6c30079132e8
cdmlonosa-svg <cdmlonosa@brc.pshs.edu.ph>
2026-09-23 22:12:36 +0800

    Update secure_registration.py

--- q1/secure_registration.py
+++ q1/secure_registration.py
@@ -1,6 +1,37 @@
-name = input("Enter student name: ").strip()
-if not name:
+student_name = input("Enter student name: ").strip()
+if not student_name:
     print("REGISTRATION NOT ACCEPTED")
-    print("Student name is required.")
+    printError: ("Student name is required.")
 else:
-    if section =! 
+    section = input("Enter section (Dahlia, Rosal, Ilang-Ilang, Sampaguita): ").strip()
+    valid_sections = ["Dahlia", "Rosal", "Ilang-Ilang", "Sampaguita"]
+    if section not in valid_sections:
+        print("REGISTRATION NOT ACCEPTED")
+        print("Error: Please enter a valid section.")
+    else:
+        club = input("Enter club (Robotics, Science, Mathematics, Programming): ").strip()
+        valid_clubs = ["Robotics", "Science", "Mathematics", "Programming"]
+        if club not in valid_clubs:
+            print("REGISTRATION NOT ACCEPTED")
+            print("Error: Please choose a valid club.")
+        else:
+            email = input("Enter school email: ").strip()
+            if "@" not in email or "." not in email:
+                print("REGISTRATION NOT ACCEPTED")
+                print("Error: Please enter a valid email.")
+            else:
+                attendance = input("Enter attendance status (Present, Absent, Late): ").strip()
+                valid_attendance = ["Present", "Absent", "Late"]
+                if attendance not in valid_attendance:
+                    print("REGISTRATION NOT ACCEPTED")
+                    print("Error: Invalid attendance status.")
+                else:
+                    print("------------------------------")
+                    print("REGISTRATION ACCEPTED")
+                    print("------------------------------")
+                    print(f"Student: {student_name}")
+                    print(f"Section: {section}")
+                    print(f"Club: {club}")
+                    print(f"Email: {email}")
+                    print(f"Attendance: {attendance}")
+
